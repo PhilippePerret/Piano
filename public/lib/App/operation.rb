efecc5e1269c496198706a116c54124c8df4b5e9
Philippe Perret <philippe.perret@yahoo.fr>
2015-02-12 00:16:40 +0100

    Opérations vote

---
+++ public/lib/App/operation.rb
@@ -0,0 +1,34 @@
+# encoding: UTF-8
+=begin
+
+Méthodes qui gèrent les opérations « o » (toutes les opérations qui ne
+peuvent pas être gérées par les vues elles-même)
+
+=end
+class App
+  
+  def opere
+    return if param('o').to_s == ""
+    require_module 'operations_o'
+    App::Operation::send( param('o').to_sym )
+  end
+  
+  ##
+  #
+  # Retourne un formulaire-boutton pour appeler l'opération
+  #
+  def form_o args
+    o   = args.delete(:o)
+    ox  = []
+    (1..10).collect do |io|
+      k = "o#{io}"
+      k_sym = k.to_sym
+      break unless args.has_key? k_sym
+      ox << args[k_sym].in_hidden(name: k)
+    end
+    art = args.delete(:article).to_s.in_hidden(name: 'article')
+    btn = (args.delete(:button) || args.delete(:titre)).in_submit(class: 'btn', onclick: args[:onclick])
+    (o.in_hidden(name: 'o') + ox.join("") + art + btn).in_form(class: 'form_o', id: args[:form_id])
+  end
+
+end
